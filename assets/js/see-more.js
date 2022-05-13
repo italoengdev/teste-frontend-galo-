879e260e828b72f0c985177856ce8ff59e042e49
italoengdev <italo_saraiva182@hotmail.com>
2022-05-13 17:12:10 -0300

    css-split

--- assets/js/see-more.js
+++ assets/js/see-more.js
@@ -4,20 +4,19 @@ function seeMore() {
   var btnText = document.getElementById('myBtn')
   var lessText = document.getElementById('less')
 
-
   if (dots.style.display !== 'none') {
     dots.style.display = 'none'
     btnText.style.display = 'none'
     moreText.style.display = 'inline'
     document.getElementById('change-body-row').style.gridTemplateRows =
-      '0.04fr 0.06fr 1.4fr 1fr 2fr 0.4fr';
-      lessText.style.display = 'inline'
-  } else{
-    dots.style.display = "inline";
-    lessText.style.display = 'none' 
-    moreText.style.display = "none";
+      '0.04fr 0.06fr 1.4fr 1fr 2.2fr 0.4fr'
+    lessText.style.display = 'inline'
+  } else {
+    dots.style.display = 'inline'
+    lessText.style.display = 'none'
+    moreText.style.display = 'none'
     document.getElementById('change-body-row').style.gridTemplateRows =
-      '0.04fr 0.06fr 1.4fr 0.6fr 2fr 0.4fr';
+      '0.04fr 0.06fr 1.4fr 0.6fr 2fr 0.4fr'
     btnText.style.display = 'inline'
   }
 }
---
+++ assets/js/submit-topic.js
@@ -0,0 +1,12 @@
+function submitTopic() {
+  var discContent = document.getElementById('topic-creation')
+  var showCreatingTopic = document.getElementById('creatingTopic')
+
+  if (discContent.style.display !== 'none') {
+    discContent.style.display = 'none'
+    showCreatingTopic.style.display = 'inline'
+    document.getElementById('change-body-row').style.gridTemplateRows =
+      '0.04fr 0.06fr 1.4fr 0.6fr 2.2fr 0.4fr'
+  } else {
+  }
+}
