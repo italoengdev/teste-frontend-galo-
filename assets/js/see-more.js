98bf1bbeb455d87672bade93b854b7146d621ff3
italoengdev <italo_saraiva182@hotmail.com>
2022-05-13 11:15:26 -0300

    create-topic-development

--- assets/js/see-more.js
+++ assets/js/see-more.js
@@ -2,6 +2,7 @@ function seeMore() {
   var dots = document.getElementById('dots')
   var moreText = document.getElementById('more')
   var btnText = document.getElementById('myBtn')
+  var lessText = document.getElementById('less')
 
 
   if (dots.style.display !== 'none') {
@@ -9,6 +10,14 @@ function seeMore() {
     btnText.style.display = 'none'
     moreText.style.display = 'inline'
     document.getElementById('change-body-row').style.gridTemplateRows =
-      '0.04fr 0.06fr 1.4fr 1fr 2fr 0.4fr'
+      '0.04fr 0.06fr 1.4fr 1fr 2fr 0.4fr';
+      lessText.style.display = 'inline'
+  } else{
+    dots.style.display = "inline";
+    lessText.style.display = 'none' 
+    moreText.style.display = "none";
+    document.getElementById('change-body-row').style.gridTemplateRows =
+      '0.04fr 0.06fr 1.4fr 0.6fr 2fr 0.4fr';
+    btnText.style.display = 'inline'
   }
 }
---
+++ assets/js/topic-creation.js
@@ -0,0 +1,10 @@
+function createTopic() {
+  var discContent = document.getElementById('topic-creation')
+
+
+  if (discContent.style.display !== 'none') {
+    discContent.style.display = 'none'
+  } else{
+    
+  }
+}
