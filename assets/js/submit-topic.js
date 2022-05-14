b817e0abf1b9ad9b487edb176b6a14c3a63f262f
italoengdev <italo_saraiva182@hotmail.com>
2022-05-14 12:23:07 -0300

    submit-finished

--- assets/js/submit-topic.js
+++ assets/js/submit-topic.js
@@ -1,12 +1,12 @@
 function submitTopic() {
-  var discContent = document.getElementById('topic-creation')
-  var showCreatingTopic = document.getElementById('creatingTopic')
+  var submitContent = document.getElementById('creatingTopic')
+  var showSubmitTopic = document.getElementById('topic-submit')
 
-  if (discContent.style.display !== 'none') {
-    discContent.style.display = 'none'
-    showCreatingTopic.style.display = 'inline'
+  if (submitContent.style.display !== 'none') {
+    submitContent.style.display = 'none'
+    showSubmitTopic.style.display = 'inline'
     document.getElementById('change-body-row').style.gridTemplateRows =
-      '0.04fr 0.06fr 1.4fr 0.6fr 2.2fr 0.4fr'
+      '0.04fr 0.06fr 1.4fr 0.6fr 2.4fr 0.4fr'
   } else {
   }
 }
