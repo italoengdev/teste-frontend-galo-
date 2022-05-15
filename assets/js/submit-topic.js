1e3ffdbcfbc109e67b3d55df1df940938668f041
italoengdev <italo_saraiva182@hotmail.com>
2022-05-14 21:39:22 -0300

    finish

--- assets/js/submit-topic.js
+++ assets/js/submit-topic.js
@@ -2,12 +2,12 @@ function submitTopic() {
   var submitContent = document.getElementById('creatingTopic')
   var showSubmitTopic = document.getElementById('topic-submit')
   var topicDefault = document.querySelector('#topic-default')
-  var divCont = `<h3>Assunto da pergunta aparece aqui</h3>
-  <h4>Carlos Henrique Santos</h4>
+  var form1 = document.querySelector('#tab-1').value
+  var form2 = document.querySelector('#tab-2').value
+  var divCont = `<h3>${form1}</h3>
+  <h4>Usuário Logado Atual</h4>
   <p>
-    Comecinho da pergunta aparece aqui resente relato inscreve-se no
-    campo da análise da dimensão e impacto de processo formativo situado
-    impacto de processo formativo processo...
+    ${form2}
   </p>
   <div class="topic-icons">
     <i class="fa-solid fa-ellipsis-vertical"></i>
@@ -20,14 +20,17 @@ function submitTopic() {
     submitContent.style.display = 'none'
     showSubmitTopic.style.display = 'inline'
     document.getElementById('change-body-row').style.gridTemplateRows =
-      '0.04fr 0.06fr 1.4fr 0.6fr 3fr 0.4fr'
+      '0.04fr 0.06fr 1.4fr 0.6fr 3.2fr 0.4fr'
     topicDefault.style.cssText = 'display:flex;'
     topicDefault.classList.add('disc-topic')
-    topicDefault.innerHTML = topicDefault.innerHTML + divCont
+    topicDefault.innerHTML += divCont
   } else {
     submitContent.style.display = 'inline'
     showSubmitTopic.style.display = 'none'
     document.getElementById('change-body-row').style.gridTemplateRows =
-      '0.04fr 0.06fr 1.4fr 0.6fr 2fr 0.4fr'
+      '0.04fr 0.06fr 1.4fr 0.6fr 3.2fr 0.4fr'
+    topicDefault.innerHTML = ''
+    topicDefault.classList.remove('disc-topic')
+    topicDefault.style.cssText = 'display:none;'
   }
 }
